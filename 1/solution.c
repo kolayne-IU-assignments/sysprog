f8676915651b49a4741149ff600818306a42037a
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2023-06-11 15:13:49 +0300

    HW1: add time measurement + minor. This is 15 points
    Measure and output time spent by coroutines and the number of context
    switches. Check if the coroutines exited correctly

--- 1/solution.c
+++ 1/solution.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "libcoro.h"
 
 #define SWAP(type, a, b) { type swap_tmp = (a); (a) = (b); (b) = swap_tmp; }
+
+inline static struct timespec timespec_sum(const struct timespec a, const struct timespec b) {
+    struct timespec sum = { .tv_sec = a.tv_sec + b.tv_sec, .tv_nsec = a.tv_nsec + b.tv_nsec };
+    return sum;
+}
+
+inline static struct timespec timespec_diff(const struct timespec a, const struct timespec b) {
+    struct timespec diff = { .tv_sec = a.tv_sec - b.tv_sec, .tv_nsec = a.tv_nsec - b.tv_nsec };
+    if (diff.tv_nsec < 0) {
+        diff.tv_nsec += 1000*1000*1000;
+        diff.tv_sec--;
+    }
+    return diff;
+}
+
+struct timespec coro_yield_timered() {
+    struct timespec start, stop;
+    if (0 != clock_gettime(CLOCK_MONOTONIC, &start)) {
+        perror("First clock_gettime(CLOCK_MONOTONIC) in coro_yield_timered");
+    }
+    coro_yield();
+    if (0 != clock_gettime(CLOCK_MONOTONIC, &stop)) {
+        perror("Second clock_gettime(CLOCK_MONOTONIC) in coro_yield_timered");
+    }
+    return timespec_diff(stop, start);
+}
 
 /**
  * Merge function. Can be used both for merging two already sorted arrays, when given subsort=false,
@@ -13,16 +40,21 @@
  * If subsort=false, the original arrays is unmodified.
  *
  * WARNING: requires that libcoro has been initialized with `coro_sched_init()` before `merge` is called.
+ *
+ * Returns a `struct timespec` that represents the total time spent sleeping in `coro_yield`.
  */
-void merge(int *out, int *from1, int len1, int *from2, int len2, bool subsort) {
+struct timespec merge(int *out, int *from1, int len1, int *from2, int len2, bool subsort) {
+    struct timespec wait_time = {0};
+
     if (subsort) {
         if (len1 > 1) {
             int *tmp = malloc(sizeof (int) * len1);
             if (!tmp) {
                 perror("Temp array malloc inside merge");
-                return;
+                return wait_time;
             }
-            merge(tmp, from1, len1/2, from1 + len1/2, len1 - len1/2, subsort);
+            struct timespec slept = merge(tmp, from1, len1/2, from1 + len1/2, len1 - len1/2, subsort);
+            wait_time = timespec_sum(wait_time, slept);
             memcpy(from1, tmp, sizeof (int) * len1);
             free(tmp);
         }
@@ -30,9 +62,10 @@ void merge(int *out, int *from1, int len1, int *from2, int len2, bool subsort) {
             int *tmp = malloc(sizeof (int) * len2);
             if (!tmp) {
                 perror("Temp array malloc inside merge");
-                return;
+                return wait_time;
             }
-            merge(tmp, from2, len2/2, from2 + len2/2, len2 - len2/2, subsort);
+            struct timespec slept = merge(tmp, from2, len2/2, from2 + len2/2, len2 - len2/2, subsort);
+            wait_time = timespec_sum(wait_time, slept);
             memcpy(from2, tmp, sizeof (int) * len2);
             free(tmp);
         }
@@ -48,44 +81,40 @@ void merge(int *out, int *from1, int len1, int *from2, int len2, bool subsort) {
 
         *out++ = mn;
         
-        coro_yield();  // Yield on every iteration
+        // Yield on every iteration
+        wait_time = timespec_sum(wait_time, coro_yield_timered());
     }
 
     while (i < from1 + len1) {
         *out++ = *i++;
-        coro_yield();  // Yield on every iteration
+        // Yield on every iteration
+        wait_time = timespec_sum(wait_time, coro_yield_timered());
     }
     while (j < from2 + len2) {
         *out++ = *j++;
-        coro_yield();  // Yield on every iteration
+        // Yield on every iteration
+        wait_time = timespec_sum(wait_time, coro_yield_timered());
     }
-}
 
-int _test_merge_main() {
-    int arr[] = {1, 5, -9, -27, 2, 6};
-    int sorted[(sizeof arr) / sizeof (int)];
-    merge(sorted, arr, (sizeof arr) / sizeof (int), NULL, 0, true);
-    for (int i = 0; i < (sizeof arr) / sizeof (int); ++i) {
-        printf("%d ", arr[i]);
-    }
-    puts("");
-    for (int i = 0; i < (sizeof arr) / sizeof (int); ++i) {
-        printf("%d ", sorted[i]);
-    }
-    puts("");
-
-    return 0;
+    return wait_time;
 }
 
 struct cor_res {
     int *array;
     int arr_size;
     int switch_count;
+    struct timespec time_spent;
 };
 
 static long long
 sort_file(void *data)
 {
+    struct timespec start;
+    if (0 != clock_gettime(CLOCK_MONOTONIC, &start)) {
+        perror("First clock_gettime(CLOCK_MONOTONIC) in sort_file");
+        return -1;
+    }
+
     const char *filename = data;
     
     FILE *f = fopen(filename, "r");
@@ -115,14 +144,14 @@ sort_file(void *data)
     // the actual amount of memory to free.
     arr_size = arr_idx;
 
-    printf("read %d numbers from %s\n", arr_size, filename);
+    (void)printf("read %d numbers from %s\n", arr_size, filename);
 
     int *sorted = malloc(sizeof (int) * arr_size);
     if (sorted == NULL) {
         perror("malloc for sorted array");
         return -1;
     }
-    merge(sorted, array, arr_size, NULL, 0, true);
+    struct timespec wait_time = merge(sorted, array, arr_size, NULL, 0, true);
     
     free(array);
 
@@ -132,9 +161,16 @@ sort_file(void *data)
         return -1;
     }
 
+    struct timespec stop;
+    if (0 != clock_gettime(CLOCK_MONOTONIC, &stop)) {
+        perror("Second clock_gettime(CLOCK_MONOTONIC) in sort_file");
+        return -1;
+    }
+
     res->array = sorted;
     res->arr_size = arr_size;
     res->switch_count = coro_switch_count(coro_this());
+    res->time_spent = timespec_diff(timespec_diff(stop, start), wait_time);
     return (long long)res;
 }
 
@@ -160,10 +196,16 @@ main(int argc, char **argv)
     struct coro *c;
     while ((c = coro_sched_wait()) != NULL) {
         struct cor_res *res = (struct cor_res *)coro_status(c);
-        results[res_idx] = *res;
-        free(res);
-        printf("Finished: %d numbers sorted\n", results[res_idx].arr_size);
-        res_idx++;
+        if ((long long)res == -1) {
+            (void)printf("Error: a coroutine terminated with an error\n");
+        } else {
+            (void)printf("Coroutine finished. Sorted %d numbers in %lld.%.9ld seconds with %d switches\n",
+                    res->arr_size, (long long)res->time_spent.tv_sec, res->time_spent.tv_nsec,
+                    res->switch_count);
+            results[res_idx] = *res;
+            free(res);
+            res_idx++;
+        }
         coro_delete(c);
     }
 
@@ -183,7 +225,7 @@ main(int argc, char **argv)
     int len1 = 0, len2 = 0;
 
     for (int i = 0; i < res_idx; ++i) {
-        merge(sorted1, sorted2, len2, results[i].array, results[i].arr_size, false);
+        (void)merge(sorted1, sorted2, len2, results[i].array, results[i].arr_size, false);
         len1 = results[i].arr_size + len2;
         free(results[i].array);  // Won't use this array again
         SWAP(int *, sorted1, sorted2);
